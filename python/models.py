6bb8a34f8a45b5e6704a42d48adde3d341e69ff4
mattkielar <matthewkielar16@gmail.com>
2022-12-03 14:50:54 -0800

    preliminary data transformation

--- python/models.py
+++ python/models.py
@@ -1,8 +1,35 @@
 # Import packages and methods
 import pandas as pd
+from pandas import DataFrame as df
 import numpy as np
 import statsmodels as sm
-import glmnet_py as glmnet
+from glmnet_py import glmnet
 import xgboost as xgb
 
-from plotnine import ggplot, aes, theme, geom_point, geom_line
+from plotnine import *
+
+# Import and transform data
+prop_prices_can = pd.read_csv("./data/can_property_prices.csv")
+income_can = pd.read_csv("./data/can_income.csv")
+
+prop_prices_can = (
+    prop_prices_can.
+    rename(columns = {"DATE": "Date", "QCAR628BIS": "Percent"})
+)
+
+prop_prices_can
+
+income_can = (
+    income_can[
+        (income_can['Statistics'] == 'Median income (excluding zeros)')].
+    filter(items = ['REF_DATE', 'VALUE']).
+    rename(columns = {"REF_DATE": "Date", "VALUE": "Income"}).
+    reset_index().
+    drop('index', axis = 1)
+)
+
+base = income_can.loc[income_can['Date'] == 2010]['Income'].values[0]
+income_can['Rebased'] = income_can['Income'] / base * 100
+
+income_can
+
